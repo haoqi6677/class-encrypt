49a9d60a3d727ad3af71e0e2a4203806f03bbeb7
haoqi <haoqi6677@qq.com>
2024-05-22 14:11:25 +0800

    1. 修改agent模块中manifest注释

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -4,11 +4,14 @@ dependencies {
 tasks.jar {
     manifest {
         attributes(
-            "Main-Class" to "fuck.world.agent.AgentMain",
-            "Agent-Class" to "fuck.world.agent.AgentMain",
+//            "Main-Class" to "fuck.world.agent.AgentMain",
+//            "Agent-Class" to "fuck.world.agent.AgentMain",
             "Premain-Class" to "fuck.world.agent.AgentMain",
+            // true表示能重定义此代理所需的类，默认值为 false（可选）
             "Can-Redefine-Classes" to "true",
+            //true 表示能重转换此代理所需的类，默认值为 false （可选）
             "Can-Retransform-Classes" to "true",
+            // true表示能设置此代理所需的本机方法前缀，默认值为 false（可选）
             "Can-Set-Native-Method-Prefix" to "true"
         )
     }
